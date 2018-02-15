b4554c98d28f28af6e815d2aa55215eace9003e6
chrisvillan <villanuevachris19@gmail.com>
2018-02-14 20:52:54 -0800

    updated committee pictures and emails

--- js/committee.js
+++ js/committee.js
@@ -1,17 +1,17 @@
 const Committee = [
   {
     id: 1,
-    image:'alvaro.png',
+    image:'alvarorivas.jpg',
     name:'Alvaro Rivas',
-    title:'A guy',
+    // title:'A guy',
     description:'Something',
-    email:'sespin@csu.fullerton.edu'
+    email:'alvaro_rivas@csu.fullerton.edu'
   },
     {
     id: 2,
     image:'stevenespin.png',
     name:'Steven Espin',
-    title:'A guy',
+    // title:'A guy',
     description:'Something',
     email:'sespin@csu.fullerton.edu'
   },
@@ -19,65 +19,66 @@ const Committee = [
     id: 3,
     image:'laurensu.png',
     name:'Lauren Sukumar',
-    title:'A guy',
+    // title:'A guy',
     description:'Something',
-    email:'email@gmail.com'
+    email:'laurensukumar@csu.fullerton.edu'
   },
     {
     id: 4,
-    image:'chrisvillan.png',
+    image:'chrisvillan.jpg',
     name:'Chris Villanueva',
-    title:'A guy',
+    // title:'A guy',
     description:'Something',
-    email:'email@gmail.com'
+    email:'cvillanueva19@csu.fullerton.edu'
   },
      {
     id: 5,
-    image:'oscar.png',
+    image:'oscar.jpg',
     name:'Oscar Olazabal',
-    title:'A guy',
+    // title:'A guy',
     description:'Something',
-    email:'email@gmail.com'
+    email:'omolazabal@csu.fullerton.edu'
   },
      {
     id: 6,
     image:'navie.png',
     name:'Navie Vurdien',
-    title:'A guy',
+    // title:'A guy',
     description:'Something',
     email:'nvurdien@csu.fullerton.edu'
   },
-     {
-    id: 7,
-    image:'charles.png',
-    name:'Charles Bucher',
-    title:'A guy',
-    description:'Something',
-    email:'email@gmail.com'
+    {
+   id: 7,
+   image:'poojagajjar.jpg',
+   name:'Pooja Gajjar',
+   // title:'A guy',
+   description:'Something',
+   email:'pkgajjar@csu.fullerton.edu'
   },
+  /*
      {
     id: 8,
-    image:'pooja.png',
-    name:'Pooja Gajjar',
-    title:'A guy',
+    image:'charles.png',
+    name:'Charles Bucher',
+    // title:'A guy',
     description:'Something',
-    email:'email@gmail.com'
+    email:'charles.abucher@gmail.com'
   },
-    
      {
     id: 9,
     image:'saqib.png',
     name:'Saqib Shahabuddin',
-    title:'A guy',
+    // title:'A guy',
     description:'Something',
-    email:'email@gmail.com'
+    email:'sshahabuddin@csu.fullerton.edu'
   },
      {
     id: 10,
     image:'tommy.png',
     name:'Tommy Chao',
-    title:'A guy',
+    // title:'A guy',
     description:'Something',
-    email:'email@gmail.com'
+    email:'tchao770@csu.fullerton.edu'
   },
+  */
 ];
